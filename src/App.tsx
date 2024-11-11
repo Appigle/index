215f94a580b406fffd942ed0cca7ea27baa07042
Chenley <chenley@qq.com>
2024-11-10 19:09:45 -0500

    feat: init project

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="flex flex-col items-center pt-[160px] h-screen w-screen bg-index-bg">
+        <h1 className="text-5xl font-bold text-purple-300 uppercase animate-pulse cursor-progress">
+          Home Of Ray
+        </h1>
+        <div className="card mt-4">
+          <button
+            onClick={() => setCount((count) => count + 1)}
+            className="text-gray-200 uppercase underline"
+          >
+            count is {count}
+          </button>
+        </div>
+      </div>
+      <a
+        href="https://github.com/Appigle"
+        title="RayChen's Github"
+        className="hover:border-purple-400 border-2 block absolute bottom-12 right-12 z-10 rounded-full"
+      >
+        <img
+          src="AVATAR_OF_DEV.jpg"
+          alt="avatar"
+          className="h-10 w-10 bg-cover rounded-full"
+        />
+      </a>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'index-bg': "url('bg-index.jpeg')",
+      },
+    },
+  },
+  plugins: [],
+};
